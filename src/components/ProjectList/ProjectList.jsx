1fb26d63c6fb8f8d5e9cbbdb628e56c29798cb15
Remezov Dmitriy <darknessdizi@gmail.com>
2024-06-28 04:19:28 +0700

    Finished project

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,15 +1,38 @@
 export const ProjectList = (props) => {
   // Функциональный компонент отрисовывающий список проектов
   let id = 0;
+  const result = {
+    first: [],
+    second: [],
+    third: [],
+  };
+  const maxCount = Math.round(props.projects.length / 3);
 
-  const result = props.projects.map((obj) => {
+  props.projects.map((obj) => {
     id += 1;
-    return <img src={obj.img} alt="photo" className="item__project" key={`project-${id}`} />;
+    if ((id > maxCount) && (id <= maxCount * 2)) {
+      result.second.push(
+        <img src={obj.img} alt="photo" className="item__project" key={`project-${id}`} />
+      );
+      return;
+    }
+    if (id <= maxCount) {
+      result.first.push(
+        <img src={obj.img} alt="photo" className="item__project" key={`project-${id}`} />
+      );
+      return;
+    }
+    return result.third.push(
+      <img src={obj.img} alt="photo" className="item__project" key={`project-${id}`} />
+    );
+    
   });
 
   return (
     <>
-    { result }
+    <div className="column">{ result.first }</div>
+    <div className="column">{ result.second }</div>
+    <div className="column">{ result.third }</div>
     </>
   )
 }
